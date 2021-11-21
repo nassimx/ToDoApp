cc5c2e213f46edc91b6a16051589c4b1ac7438b0
Nassim Dev <mr.nassimfr@gmail.com>
2021-11-21 12:01:10 +0100

    rm devtool

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,13 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './Reducer';
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(
-    applyMiddleware()
-    // other store enhancers if any
-  )
+  applyMiddleware()
+  // other store enhancers if any
 );
 export default store;
